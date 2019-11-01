cde89ecb964a6c53f52fcdb80148edf7a3890b88
Art Navsegda <artnavsegda@gmail.com>
2019-11-01 11:08:44 +0300

    a

--- run-clean.sh
+++ run-clean.sh
@@ -2,5 +2,5 @@
 make clean
 make distclean
 rm -rvf aclocal.m4 autom4te.cache compile config.h config.h.in config.log config.status configure depcomp install-sh Makefile Makefile.in missing stamp-h1 config.guess config.sub
-rm src/Makefile.in src/io/Makefile.in src/serial/Makefile.in src/settings/Makefile.in src/signal/Makefile.in src/sockets/Makefile.in src/webserver/Makefile.in src/thread/Makefile.in
+rm src/Makefile.in
 
